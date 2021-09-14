9b618f37f218cf368210a83d8f0a563486855d43
you-bowen <2756456886@qq.com>
2021-09-14 15:02:57 +0800

    add websites

--- cSource.sh
+++ cSource.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # 需要换源的程序
 # pip docker apt brew npm 
+# 三大镜像站
+# 阿里   https://developer.aliyun.com/mirror/
+# 清华   https://mirrors.tuna.tsinghua.edu.cn/
+# 中科大 https://mirrors.ustc.edu.cn/
