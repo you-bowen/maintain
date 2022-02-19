0857d7fec2b1564351d28a623c4b4da9abacdfdc
you-bowen <2756456886@qq.com>
2022-02-20 00:50:45 +0800

    ..

--- plugins/install.py
+++ plugins/install.py
@@ -1,6 +1,5 @@
 import os
-cwd = __file__[:__file__.rindex("/")]
-print(cwd)
+cwd = f"{os.getenv('HOME')}/maintain/plugins"
 root = f"{os.getenv('HOME')}/.oh-my-zsh/plugins"
 for file in os.listdir(cwd):
     if not file.endswith(".plugin.zsh"):
@@ -9,5 +8,6 @@ for file in os.listdir(cwd):
     if os.path.exists(f"{root}/{plugin_name}"):
         print(f"dir-`{plugin_name}` exists") # 有可能你写的插件和已经有的插件重名了
     else:
+        # 如果不存在，创建文件夹，建立链接
         os.mkdir(f"{root}/{plugin_name}")
         os.system(f"ln -s {cwd}/{file} {root}/{plugin_name}/{file}")
